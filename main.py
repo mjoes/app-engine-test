158fcbf0a3bca7411bc0bfa828190a0c4f288f7f
mjoes <morten.slingsby@gmail.com>
2023-03-22 22:40:17 +0100

    save

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import time
 from loguru import logger
+from google.cloud import storage
 
-def function_test():
-    i=0
-    k=0
-    while i < 10:
-        k=k+1
-        time.sleep(1)
-        logger.info(f"logging {k}")
-        print(f"test print no: {k}")
+client = storage.Client()
+bucket = client.bucket('auphonic-storage')
+for blob in bucket.list_blobs(prefix='auphonic/'):
+    if blob.name == 'auphonic/':
+        pass
+    else:
+        print(blob.name)
 
-function_test()
+
+# def function_test():
+#     i=0
+#     k=0
+#     while i < 10:
+#         k=k+1
+#         time.sleep(1)
+#         logger.info(f"logging {k}")
+#         print(f"test print no: {k}")
+
+# function_test()
