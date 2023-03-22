c51f9aa832b89775c93a5fe4277b00489119f804
mjoes <morten.slingsby@gmail.com>
2023-03-22 23:19:58 +0100

    save

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,5 +1,6 @@
 sudo systemctl stop apptest.service
 cd /home/johnpeel/app-engine-test/
+git config --global --add safe.directory /home/johnpeel/app-engine-test
 git pull
 sudo pip3 install --upgrade -r requirements.txt
 sudo cp /home/johnpeel/app-engine-test/apptest.service /etc/systemd/system/apptest.service
