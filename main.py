8a8cb9b0f6ec156cf1f302cd191c7042880df659
mjoes <morten.slingsby@gmail.com>
2023-03-22 19:20:19 +0100

    save

---
+++ main.py
@@ -0,0 +1,11 @@
+import time
+import logging 
+i=0
+k=0
+
+def function_test():
+    while i < 10:
+        k=k+1
+        time.sleep(1)
+        logging.info(f"logging {k}")
+        print(f"test print no: {k}")
