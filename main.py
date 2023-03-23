880d0040444757c82d8d8c69b70773738274b712
mjoes <morten.slingsby@gmail.com>
2023-03-23 21:24:18 +0100

    save

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from loguru import logger
 from google.cloud import storage
 import gspread
 import google.auth
+import os
 
 client = storage.Client()
 bucket = client.bucket('auphonic-storage')
@@ -12,20 +13,17 @@ for blob in bucket.list_blobs(prefix='auphonic/'):
     else:
         print(blob.name)
 
+## part 2
 
 SHEET_ID="1ZDPKh4zHLKUIqNo9XP9HMjxSd1TJwvTa_bjlEMY6m9M"
 SHEET_NAME="meta"
+filepath = 'home/dev1/sheetsapi.json'
+# scopes=[
+#     'https://www.googleapis.com/auth/spreadsheets',
+#     'https://www.googleapis.com/auth/drive'
+# ]
 
-import gspread
-
-credentials, project_id = google.auth.default(
-    scopes=[
-        'https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/drive'
-    ]
-)
-print(credentials)
-gc = gspread.authorize(credentials)
+gc = gspread.service_account(filename=filepath)
 # gc = gspread.oauth()
 
 spreadsheet = gc.open_by_key(SHEET_ID)
@@ -35,13 +33,3 @@ rows = worksheet.get_all_records()
 print(rows)
 print("DONE NOW")
 
-# def function_test():
-#     i=0
-#     k=0
-#     while i < 10:
-#         k=k+1
-#         time.sleep(1)
-#         logger.info(f"logging {k}")
-#         print(f"test print no: {k}")
-
-# function_test()
